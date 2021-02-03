075c70f419c31d9b984402cf8052da5b9336fe5c
minhaz-bin-monib <minhazbm463448@gmail.com>
2021-02-03 19:19:55 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// falsy
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0',' ', []
+name = null;
+if(name){
+    console.log('condition is true');
+}
+else{ 
+    console.log('condition is false');
+}
