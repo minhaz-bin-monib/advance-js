cf2e15bc99375d16a9289da2a34eee45f1069f71
minhaz-bin-monib <minhazbm463448@gmail.com>
2021-02-03 21:11:59 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let value;
+// function add(num1,num2){
+//     console.log(num1+num2)
+return
+// }
+function add(num1,num2){
+        console.log(num1,num2);
+    
+    }
+const result = add(13) ;
+
+const myInfo = {nickName:"minhaz", phoneNumber: 437057}
+console.log(myInfo.address);
+let fun = undefined;
+console.log(fun);
+
+const num = [4,4,5,2];
+console.log(num[6]);
